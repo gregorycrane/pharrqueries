7991c1fa3b7a8d05b17728980f26fd8bbaab84c2
bellahwang15 <44103742+bellahwang15@users.noreply.github.com>
2020-06-01 15:51:45 -0400

    update

--- queryscripts/vocab_query.py
+++ queryscripts/vocab_query.py
@@ -1,12 +1,21 @@
 import sys
 
 import xml.etree.ElementTree as ET
-tree = ET.parse('/Users/bellahwang/Documents/GitHub/gAGDT/data/xml/tlg0012.tlg001.perseus-grc1.tb.xml')
+tree = ET.parse('/mnt/c/Users/bella/OneDrive/Documents/GitHub/gAGDT/data/xml/tlg0012.tlg001.perseus-grc1.tb.xml')
 root = tree.getroot()
 
-#tree1 = ET.parse('/Users/bellahwang/Documents/GitHub/Homerica/murray.iliad3.xml')
-#root1 = tree1.getroot()
+tree1 = ET.parse('/mnt/c/Users/bella/OneDrive/Documents/GitHub/Homerica/murray.iliad3.xml')
+root1 = tree1.getroot()
 
+def printEngSent(target):
+    for TEI in root1.findall('./TEI'):
+ #   for body in root1.findall('./body'):
+        print("hi")
+"""         for p in body.findall('./p'):
+            for sentence in p.findall('./s'):
+                id = sentence.get('xml:id')
+                id[4:10]
+                print(id) """
 
 # prints out entire sentence 
 def printSent(target):
@@ -14,10 +23,10 @@ def printSent(target):
     for body in root.findall('./body'): 
         for sentence in body.findall('./sentence'):
             
-            # Takes in subdoc value
-            subdoc = sentence.get('subdoc')
+            # Takes in id value
+            id = sentence.get('id')
 
-            if (subdoc == target):
+            if (id == target):
                 for word in sentence.findall('./word'):
                     form = word.get('form')
                     postag = word.get('postag')
@@ -38,6 +47,8 @@ for body in root.findall('./body'):
 
         # Takes in subdoc value
         subdoc = sentence.get('subdoc')
+        # Takes in id value
+        id = sentence.get('id')
 
         # Sets bool value for whether the subdoc is within Iliad Book 1
         isBookOne = subdoc.startswith('1.')
@@ -50,7 +61,8 @@ for body in root.findall('./body'):
 
                 if (vocab == lemma):
                     print(count, "form: ", form, "lemma: ", lemma, cite)
-                    printSent(subdoc)
+                    printSent(id)
+                    printEngSent(id)
                     print('')
                     print('')
                     count += 1
