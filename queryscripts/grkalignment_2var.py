405d0e784efc0092fe6af6a75d2e38b503477ccc
bellahwang15 <44103742+bellahwang15@users.noreply.github.com>
2020-06-18 21:23:30 -0400

    subjunctive_query edit
    now prints out greek and english alignments, greek alignment highlights verb form and ἄν form that modifies it

---
+++ queryscripts/grkalignment_2var.py
@@ -0,0 +1,30 @@
+from pharr_vocab import printVocabSection
+import sys
+import xml.etree.ElementTree as ET
+#tree = ET.parse('/mnt/c/Users/bella/OneDrive/Documents/GitHub/gAGDT/data/xml/tlg0012.tlg001.perseus-grc1.tb.xml')
+tree = ET.parse('/Users/bellahwang/Documents/GitHub/gAGDT/data/xml/tlg0012.tlg001.perseus-grc1.tb.xml')
+root = tree.getroot()
+
+# prints out entire sentence 
+def printGrkSent(target, firstform, secondform):
+
+    for body in root.findall('./body'): 
+        for sentence in body.findall('./sentence'):
+            
+            # Takes in id value
+            id = sentence.get('id')
+
+            if (id == target):
+                for word in sentence.findall('./word'):
+                    form = word.get('form')
+                    lemma = word.get('lemma')
+                    postag = word.get('postag')
+                    if (postag != None):
+                        if (form == firstform or form == secondform):
+                                            #  bold        green               end
+                            sys.stdout.write('\033[1m' + '\33[32m' + form + '\033[0m')
+                            sys.stdout.write(" ")
+                            printVocabSection(lemma)
+                            continue
+                        sys.stdout.write(form)
+                        sys.stdout.write(" ")
--- queryscripts/subjunctivequery.py
+++ queryscripts/subjunctivequery.py
@@ -1,25 +1,38 @@
+from englishalignment import printEngSent
+from grkalignment_2var import printGrkSent
 import xml.etree.ElementTree as ET
-tree = ET.parse("/Users/bellahwang/Documents/GitHub/gAGDT/data/xml/tlg0012.tlg002.perseus-grc1.tb.xml")
+tree = ET.parse("/Users/bellahwang/Documents/GitHub/gAGDT/data/xml/tlg0012.tlg001.perseus-grc1.tb.xml")
 root = tree.getroot()
 
-count=1		#sets counter keeping track of query results
-subjid=None 		#in case the code encounters a subjunctive first
-anhead=None		#in case the code encounters an ἄν first
+count = 1		    #sets counter keeping track of query results
+subjid = None 		#in case the code encounters a subjunctive first
+anhead = None		#in case the code encounters an ἄν first
+anform = None
 
 for body in root.findall('./body'): 
 	for sentence in body.findall('./sentence'):
-		print(sentence.attrib) 
+        # Takes in subdoc value
+		subdoc = sentence.get('subdoc')
+        # Takes in id value
+		id = sentence.get('id')
 		for word in sentence.findall('./word'):
 			if 'postag' in word.attrib:
 				postag = word.get('postag')
 				tense = postag[4] #narrows down to verb tense
+				form = word.get('form')
+				cite = word.get('cite')
 				lemma = word.get('lemma')
 				if lemma == 'ἄν':
 					anhead = word.get('head')
+					anform = word.get('form')
 				else: #only other possibility is if the word is a subjunctive
 					subjid = word.get('id')
 					if anhead == subjid: #checks if ἄν modifies subjunctive
 						if tense == 's':
-							print(word.attrib)
-							print(count) #counts query results
+							print(count, "form: ", form, "lemma: ", lemma, "cite: ", cite)
+							printGrkSent(id, form, anform)
+							print('')
+							printEngSent(id)
+							print('')
+							print('')
 							count+=1
