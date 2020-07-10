db894c03cfa866cbce95d6abc968e3737b90b8de
bellahwang15 <44103742+bellahwang15@users.noreply.github.com>
2020-07-10 12:33:07 -0400

    quotes now include highlighted forms

--- queryscripts/cunliffe/cunliffelexentries.py
+++ queryscripts/cunliffe/cunliffelexentries.py
@@ -1,6 +1,8 @@
 from iliad import returnIliad
 from odyssey import returnOdyssey
+from returnForms import returnForms
 import sys
+import re
 import xml.etree.ElementTree as ET
 
 # change FILENAME to local path
@@ -15,16 +17,44 @@ def parseLexEntries(input):
 		wordEntry = entries.get('n')
 		if (wordEntry != None):
 			if (wordEntry == input):
-				for gloss in entries.findall(".//TEI:gloss", ns):
-					print("Gloss:", gloss.text)
-					for bibl in entries.findall(".//TEI:gloss/../TEI:bibl", ns):
-						biblEntry = bibl.get('n')
-						print(biblEntry)
-						if (biblEntry.startswith('Hom. Il.')):
-							returnIliad(wordEntry, biblEntry)
-							print("")
-							print("")
-						else:
-							returnOdyssey(wordEntry, biblEntry)
-							print("")
-							print("")
+				for p in entries.findall('.//TEI:p', ns):
+					for gloss in p.findall(".//TEI:gloss", ns):
+						print("Gloss:", gloss.text)
+						for quote in p.findall(".//TEI:gloss/../TEI:cit/TEI:quote", ns):
+							sys.stdout.write("Quote: ")
+							splitQuote = re.split("\s", quote.text)
+							forms = returnForms(input)
+							for i in splitQuote:
+								for j in forms:
+									if (i == j):
+														#  bold        green               end
+										sys.stdout.write('\033[1m' + '\33[32m' + i + '\033[0m')
+										sys.stdout.write(" ")
+										break;
+								if (i == j):
+									continue
+								else:
+									sys.stdout.write(i)
+									sys.stdout.write(" ")
+							for bibl in p.findall(".//TEI:gloss/../TEI:cit/TEI:bibl", ns):
+								biblEntry = bibl.get('n')
+								print("(" + biblEntry + ")")
+								if (biblEntry.startswith('Hom. Il.')):
+									returnIliad(wordEntry, biblEntry)
+									print("")
+									print("")
+								else:
+									returnOdyssey(wordEntry, biblEntry)
+									print("")
+									print("")
+				for bibl in entries.findall(".//TEI:gloss/../TEI:bibl", ns):
+					biblEntry = bibl.get('n')
+					print(biblEntry)
+					if (biblEntry.startswith('Hom. Il.')):
+						returnIliad(wordEntry, biblEntry)
+						print("")
+						print("")
+					else:
+						returnOdyssey(wordEntry, biblEntry)
+						print("")
+						print("")
